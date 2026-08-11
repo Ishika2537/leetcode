10e036d54f6a1e8f553990c1a2490b1b2c2073fc
Ishika <ishikakri253@gmail.com>
2026-08-11 16:04:51 +0530

    Time: 1 ms (99.96%) | Memory: 94.3 MB (71.56%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -11,16 +11,4 @@ class Solution {
         }
         return profit; 
     }
-    public static void main(String [] args)
-    {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int [] arr=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
-        }
-        Solution sol=new Solution();
-        System.out.println(sol.maxProfit(arr));
-    }
 }
